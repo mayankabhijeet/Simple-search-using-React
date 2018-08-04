aab8cc7c02033b6025cea5886d5328a5721e1d5b
mayank abhijeet <mayank16abhijeet@gmail.com>
2018-08-05 02:29:57 +0530

    simple search using react

---
+++ src/components/header.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      keyword: ''
+    }
+    console.log(props)
+  }
+
+  inputChange(event) {
+    this.setState({keyword:event.target.value});
+    this.props.newsSearch(event.target.value)
+  }
+
+  render() {
+    return (
+      <header>
+        <div
+          className="logo"
+          onClick={() => {
+            console.log("yayyyy! its working");
+          }}
+        >
+          Logo
+        </div>
+        <input onChange={this.inputChange.bind(this)} />
+      </header>
+    );
+  }
+}
+
+export default Header;
